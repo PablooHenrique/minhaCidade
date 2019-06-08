8e9ca617138bdd81f314b0f124a3809ef085a925
mateusbw <mateus_bwilding@hotmail.com>
2019-06-08 19:35:51 -0300

    Login

--- minhaCidade/src/app/root/root.module.ts
+++ minhaCidade/src/app/root/root.module.ts
@@ -6,14 +6,16 @@ import {FormsModule} from '@angular/forms';
 import {RootPageRoutingModule} from './root.router.module';
 import {RootPage} from './root.page';
 import {UserCreatePage} from '../user/user-create/user-create.page';
+import { Tab1Page } from '../tab1/tab1.page';
 
 @NgModule({
 	imports: [
 		IonicModule,
 		CommonModule,
 		FormsModule,
-		RootPageRoutingModule
+		RootPageRoutingModule,
+		
 	],
-	declarations: [RootPage, UserCreatePage]
+	declarations: [RootPage, UserCreatePage, Tab1Page]
 })
 export class RootPageModule {}
--- minhaCidade/src/app/root/root.router.module.ts
+++ minhaCidade/src/app/root/root.router.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {TabsPage} from '../tabs/tabs.page';
 import {RootPage} from './root.page';
 import {UserCreatePage} from '../user/user-create/user-create.page';
+import { Tab1Page } from '../tab1/tab1.page';
 
 const routes: Routes = [
 	{
@@ -12,6 +13,10 @@ const routes: Routes = [
 	{
 		path: 'user-create',
 		component: UserCreatePage,
+	},
+	{
+		path: 'tab-1',
+		component: Tab1Page,
 	},
 	{
 		path: '',
