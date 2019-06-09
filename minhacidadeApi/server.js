f6afcf9f59b811c38f273dc87715ed5ab7bba0c7
Anuar <anuar_daher@hotmail.com>
2019-06-09 04:05:24 -0300

    trocando a porta da api

--- minhacidadeApi/server.js
+++ minhacidadeApi/server.js
@@ -1,6 +1,6 @@
 const app = require("./config/express")
 require("./src/routes/auth")(app)
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Express server is listening on port ${port}`));
