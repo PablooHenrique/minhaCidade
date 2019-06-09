7a5157882cc4c56b61839a2404dc7fac4db1d3bd
Pablo Henrique <pabloohenrique32@gmail.com>
2019-06-09 15:31:15 -0300

    Adicionando toastContoller

--- minhaCidade/src/app/desafio/create/desafios-create.page.ts
+++ minhaCidade/src/app/desafio/create/desafios-create.page.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {ToastController} from '@ionic/angular';
 
 @Component({
 	selector: 'app-desafio-create',
@@ -7,4 +9,20 @@ import {Component} from '@angular/core';
 })
 export class DesafiosCreatePage {
 
+	constructor(private _router: Router, private _toast: ToastController) {}
+
+	salvar() {
+		this.showToast('Desafio adicionado com sucesso');
+		this._router.navigate(['/desafio-list']);
+	}
+
+	async showToast(msg: string) {
+		const toast = await this._toast.create({
+			message: msg,
+			duration: 2000,
+			position: 'top'
+		});
+
+		toast.present();
+	}
 }
